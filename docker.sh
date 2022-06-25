b92202ad4c8bc96b304047995a7064115d9ca45a
shhan <shhan0226@gmail.com>
2022-06-25 16:16:25 +0000

    update docker.sh

--- docker.sh
+++ docker.sh
@@ -28,3 +28,32 @@ sudo apt-get install -y docker-ce=5:20.10.17~3-0~ubuntu-focal docker-ce-cli=5:20
 
 #도커 테스트
 sudo docker run hello-world
+
+# swap 제거
+swapoff -a
+
+# cgroup 변경
+touch daemon.json
+cat > daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo chown root:root daemon.json
+sudo mv ./daemon.json /etc/docker/.
+
+# docker service
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl enable docker
+
+
+# grub update
+sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cgroup_enable=memory swapaccount=1\"/" /etc/default/grub
+update-grub
+shutdown -r now
+
